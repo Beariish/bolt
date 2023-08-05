3c358d9b7a0055046cb79000077fde9124bc5029
Beariish <niklas98956@gmail.com>
2023-08-05 19:03:41 +0100

    Actually included said file...

---
+++ bt_config.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// This header exists in order to configure some parts of bolts functionality, it is included in the relevant places
+// Whether or not these are enabled shouldn't intergere with ABI boundary, but behvaiour may vary between compilers
+
+// Use explicit butmasking in order to utilize otherwise-zero'd bits inside the GC next pointer
+// This reduces the size of ALL BOLT OBJECTS by 8 bytes, but does make debugging more challenging
+// as it's impossible to really inspect the GC state
+#define BOLT_USE_MASKED_GC_HEADER
+
+// This replaces the union struct normally used to represent bolt operations with bitmasked integers instead
+// On MSVC specifically, this gives measurable speedup. Other platforms may vary.
+// This does also make debugging more tedious though.
+#define BOLT_BITMASK_OP
+
+// Enables more debug printing throughout bolt execution, dumping things like token stream, ast state, and 
+// compiled bytecode to the console.
+// #defibe BOLT_PRINT_DEBUG
