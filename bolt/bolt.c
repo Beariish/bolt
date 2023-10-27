8d5bbd6b4732a0edd7a876b7c83793278025d92f
Beariish <niklas98956@gmail.com>
2023-10-27 17:01:26 +0100

    Fix for bt_call

--- bolt/bolt.c
+++ bolt/bolt.c
@@ -613,7 +613,7 @@ void bt_call(bt_Thread* thread, uint8_t argc)
 	thread->top += frame->size + 2;
 	bt_Object* obj = BT_AS_OBJECT(thread->stack[thread->top - 1]);
 
-	thread->callstack[thread->depth].return_loc = -1;
+	thread->callstack[thread->depth].return_loc = -2;
 	thread->callstack[thread->depth].argc = argc;
 	thread->callstack[thread->depth].callable = (bt_Callable*)obj;
 	thread->callstack[thread->depth].user_top = 0;
@@ -623,12 +623,12 @@ void bt_call(bt_Thread* thread, uint8_t argc)
 	case BT_OBJECT_TYPE_FN: {
 		bt_Fn* callable = (bt_Fn*)obj;
 		thread->callstack[thread->depth - 1].size = callable->stack_size;
-		call(thread->context, thread, callable->module, callable->instructions.elements, callable->constants.elements, -1);
+		call(thread->context, thread, callable->module, callable->instructions.elements, callable->constants.elements, -2);
 	} break;
 	case BT_OBJECT_TYPE_CLOSURE: {
 		bt_Fn* callable = ((bt_Closure*)obj)->fn;
 		thread->callstack[thread->depth - 1].size = callable->stack_size;
-		call(thread->context, thread, callable->module, callable->instructions.elements, callable->constants.elements, -1);
+		call(thread->context, thread, callable->module, callable->instructions.elements, callable->constants.elements, -2);
 	} break;
 	case BT_OBJECT_TYPE_NATIVE_FN: {
 		bt_NativeFn* callable = (bt_NativeFn*)obj;
@@ -637,7 +637,7 @@ void bt_call(bt_Thread* thread, uint8_t argc)
 	case BT_OBJECT_TYPE_MODULE: {
 		bt_Module* mod = (bt_Module*)obj;
 		thread->callstack[thread->depth - 1].size = mod->stack_size;
-		call(thread->context, thread, mod, mod->instructions.elements, mod->constants.elements, -1);
+		call(thread->context, thread, mod, mod->instructions.elements, mod->constants.elements, -2);
 	} break;
 	default: bt_runtime_error(thread, "Unsupported callable type.", NULL);
 	}
