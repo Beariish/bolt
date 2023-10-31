64d5f9fe92158fdf448db47c6aefc7aea110b47c
Beariish <niklas98956@gmail.com>
2023-10-31 10:17:43 +0000

    Spaces are important

--- bolt/bt_op.h
+++ bolt/bt_op.h
@@ -9,59 +9,59 @@
 */
 
 #define BT_OPS_X                                                                    \
-	X(LOAD)        /*  R(a) = L[ubc]                                 */             \
-	X(LOAD_SMALL)  /*  R(a) = tonumber(ibc)                          */             \
-	X(LOAD_NULL)   /*  R(a) = null                                   */             \
-	X(LOAD_BOOL)   /*  R(a) = b ? BT_TRUE : BT_FALSE                 */             \
-	X(LOAD_IMPORT) /*  R(a) = imports[ubc]                           */             \
-	X(TABLE)       /*  R(a) = new tablesize(ibc)                     */             \
-	X(ARRAY)       /*  R(a) = new array[b]                           */             \
-	X(MOVE)        /*  R(a) = R(b)                                   */             \
-	X(EXPORT)      /*  exports[R(a)]: R(c) = R(b)                    */             \
-	X(CLOSE)       /*  R(a) = Closure(R(b)) with upvals[R(b+1..b+c)] */             \
-	X(LOADUP)      /*  R(a) = upvals[b]                              */             \
-	X(STOREUP)     /*  upvals[a] = R(b)                              */             \
-	X(NEG)         /*  R(a) = -R(b)                                  */             \
-	X(ADD)         /*  R(a) = R(b) + R(c)                            */             \
-	X(SUB)         /*  R(a) = R(b) - R(c)                            */             \
-	X(MUL)         /*  R(a) = R(b) * R(c)                            */             \
-	X(DIV)         /*  R(a) = R(b) / R(c)                            */             \
-	X(EQ)          /*  R(a) = R(b) == R(c)                           */             \
-	X(NEQ)         /*  R(a) = R(b) != R(c)                           */             \
-	X(LT)          /*  R(a) = R(b) < R(c)                            */             \
-	X(LTE)         /*  R(a) = R(b) <= R(c)                           */             \
-	X(AND)         /*  R(a) = R(b) and R(c)                          */             \
-	X(OR)          /*  R(a) = R(b) or R(c)                           */             \
-	X(NOT)         /*  R(a) = not R(b)                               */             \
-	X(LOAD_IDX)    /*  R(a) = R(b).[R(c)]                            */             \
-	X(STORE_IDX)   /*  R(b).[R(c)] = R(a)                            */             \
-	X(LOAD_IDX_K)  /*  R(a) = R(b).[L(c)]                            */             \
-	X(STORE_IDX_K) /*  R(b).[L(c)] = R(a)                            */             \
-	X(EXPECT)      /*  R(a) = R(b) ? FAIL                            */             \
-	X(EXISTS)      /*  R(a) = R(b) != null                           */             \
-	X(COALESCE)    /*  R(a) = R(b) == null ? R(c) : R(b)             */             \
-	X(TCHECK)      /*  R(a) = R(b) is Type(c)                        */             \
-	X(TSATIS)      /*  R(a) = R(b) satisfies Type(c)                 */             \
-	X(TCAST)       /*  R(a) = R(b) as Type(c)                        */             \
-	X(TSET)        /*  (R(a) as Type)[R(b)]: R(c+1) = R(c)           */             \
-	X(COMPOSE)     /*  R(a) = fieldsof(b) + fieldsof(c)              */             \
-	X(CALL)        /*  R(a) = R(b)(R(b + 1) .. R(b + c))             */             \
-	X(JMP)         /*  pc += ibc                                     */             \
-	X(JMPF)        /*  if(R(a) == BT_FALSE) pc += ibc                */             \
-	X(RETURN)      /*  R(frame->ret_pos) = R(a)                      */             \
-	X(END)         /*  return without value                          */             \
+    X(LOAD)        /*  R(a) = L[ubc]                                 */             \
+    X(LOAD_SMALL)  /*  R(a) = tonumber(ibc)                          */             \
+    X(LOAD_NULL)   /*  R(a) = null                                   */             \
+    X(LOAD_BOOL)   /*  R(a) = b ? BT_TRUE : BT_FALSE                 */             \
+    X(LOAD_IMPORT) /*  R(a) = imports[ubc]                           */             \
+    X(TABLE)       /*  R(a) = new tablesize(ibc)                     */             \
+    X(ARRAY)       /*  R(a) = new array[b]                           */             \
+    X(MOVE)        /*  R(a) = R(b)                                   */             \
+    X(EXPORT)      /*  exports[R(a)]: R(c) = R(b)                    */             \
+    X(CLOSE)       /*  R(a) = Closure(R(b)) with upvals[R(b+1..b+c)] */             \
+    X(LOADUP)      /*  R(a) = upvals[b]                              */             \
+    X(STOREUP)     /*  upvals[a] = R(b)                              */             \
+    X(NEG)         /*  R(a) = -R(b)                                  */             \
+    X(ADD)         /*  R(a) = R(b) + R(c)                            */             \
+    X(SUB)         /*  R(a) = R(b) - R(c)                            */             \
+    X(MUL)         /*  R(a) = R(b) * R(c)                            */             \
+    X(DIV)         /*  R(a) = R(b) / R(c)                            */             \
+    X(EQ)          /*  R(a) = R(b) == R(c)                           */             \
+    X(NEQ)         /*  R(a) = R(b) != R(c)                           */             \
+    X(LT)          /*  R(a) = R(b) < R(c)                            */             \
+    X(LTE)         /*  R(a) = R(b) <= R(c)                           */             \
+    X(AND)         /*  R(a) = R(b) and R(c)                          */             \
+    X(OR)          /*  R(a) = R(b) or R(c)                           */             \
+    X(NOT)         /*  R(a) = not R(b)                               */             \
+    X(LOAD_IDX)    /*  R(a) = R(b).[R(c)]                            */             \
+    X(STORE_IDX)   /*  R(b).[R(c)] = R(a)                            */             \
+    X(LOAD_IDX_K)  /*  R(a) = R(b).[L(c)]                            */             \
+    X(STORE_IDX_K) /*  R(b).[L(c)] = R(a)                            */             \
+    X(EXPECT)      /*  R(a) = R(b) ? FAIL                            */             \
+    X(EXISTS)      /*  R(a) = R(b) != null                           */             \
+    X(COALESCE)    /*  R(a) = R(b) == null ? R(c) : R(b)             */             \
+    X(TCHECK)      /*  R(a) = R(b) is Type(c)                        */             \
+    X(TSATIS)      /*  R(a) = R(b) satisfies Type(c)                 */             \
+    X(TCAST)       /*  R(a) = R(b) as Type(c)                        */             \
+    X(TSET)        /*  (R(a) as Type)[R(b)]: R(c+1) = R(c)           */             \
+    X(COMPOSE)     /*  R(a) = fieldsof(b) + fieldsof(c)              */             \
+    X(CALL)        /*  R(a) = R(b)(R(b + 1) .. R(b + c))             */             \
+    X(JMP)         /*  pc += ibc                                     */             \
+    X(JMPF)        /*  if(R(a) == BT_FALSE) pc += ibc                */             \
+    X(RETURN)      /*  R(frame->ret_pos) = R(a)                      */             \
+    X(END)         /*  return without value                          */             \
 	                                                                                \
-	/*  Fast opcode extensions. */                                                  \
-	/*  These are emitted by the compiler whenever types are strongly known */      \
+    /*  Fast opcode extensions. */                                                  \
+    /*  These are emitted by the compiler whenever types are strongly known */      \
                                                                                     \
-	/*  Looping macroops */                                                         \
-	X(NUMFOR)                                                                       \
-	X(ITERFOR)                                                                      \
+    /*  Looping macroops */                                                         \
+    X(NUMFOR)                                                                       \
+    X(ITERFOR)                                                                      \
                                                                                     \
-	/*  Fast array indexing. Used when the indexed type is known to be an array, */	\
-	/*  and the index known to be a number */                                       \
-	X(LOAD_SUB_F)                                                                   \
-	X(STORE_SUB_F)
+    /*  Fast array indexing. Used when the indexed type is known to be an array, */ \
+    /*  and the index known to be a number */                                       \
+    X(LOAD_SUB_F)                                                                   \
+    X(STORE_SUB_F)
 
 typedef enum {
 #define X(op) BT_OP_##op,
