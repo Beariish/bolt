9249a6ce6fab2869c56b926b17bcbbc4345dc60c
Beariish <niklas98956@gmail.com>
2023-12-22 03:44:55 +0000

    Bump version number

--- bolt/bt_prelude.h
+++ bolt/bt_prelude.h
@@ -5,8 +5,8 @@
 #include <stddef.h>
 
 #define BOLT_VERSION_MAJOR 0
-#define BOLT_VERSION_MINOR 0
-#define BOLT_VERSION_REVISION 1
+#define BOLT_VERSION_MINOR 1
+#define BOLT_VERSION_REVISION 0
 
 #define BOLT_STR(x) #x
 #define BOLT_XSTR(x) BOLT_STR(x)
